2f4149f22d6deecdc03ae58b4df90ac9e4d1e669
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-04-25 21:00:57 +0200

    feat: created basic venue card skeleton

--- src/components/VenueCard.tsx
+++ src/components/VenueCard.tsx
@@ -1,6 +1,27 @@
-import React from "react";
-import Venue from "../types/venue";
+import Venue from "../types/Venue";
 
-interface VenueCardProps {
+interface IVenueCardProps {
   venue: Venue;
 }
+
+function VenueCard({ venue }: IVenueCardProps) {
+  let imageContent;
+  if (venue.media.length > 0) {
+    imageContent = <img src={venue.media[0].url} alt={venue.media[0].alt} />;
+  } else {
+    imageContent = <div>No image available</div>;
+  }
+
+  return (
+    <div className="venue-card" key={venue.id}>
+      {imageContent}
+      <h2>{venue.name}</h2>
+      <p>Price: {venue.price}kr</p>
+      <p>Max Guests: {venue.maxGuests}</p>
+      <p>Rating: {venue.rating}</p>
+      <p>Location: {venue.location.country}</p>
+    </div>
+  );
+}
+
+export default VenueCard;
