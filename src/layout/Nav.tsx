ba92a2c9f19867ab425f0634ae5e34eeb1004ad7
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-05-06 22:37:53 +0200

    feat: add searchbar to navbar

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
+import SearchBar from "../components/search";
+
 function Navbar() {
   return (
     <header>
       <nav>
         <p>HOLIDAZE</p>
+        <SearchBar />
         <NavLink to="/">Home</NavLink>
         <NavLink to="/Venues">All Venues</NavLink>
         <NavLink to="/DashboardManager">Dashboard Manager</NavLink>
