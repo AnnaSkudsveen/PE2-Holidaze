4be5a84e6f04268f69b8a06b3bc0872f9757f916
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-05-09 22:57:46 +0200

    chore: add API endpoints to a constant file

---
+++ src/constants/api.tsx
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "https://v2.api.noroff.dev";
+
+export const ENDPOINTS = {
+  LOGIN: "/auth/login?holidaze=true",
+  VENUES: "/holidaze/venues",
+  BOOKINGS: "/holidaze/bookings",
+  PROFILES: "/holidaze/profiles",
+  PROFILES_BOOKINGS: "/holidaze/profiles/{name}/bookings",
+  PROFILES_VENUES: "/holidaze/profiles/{name}/venues"
+};
