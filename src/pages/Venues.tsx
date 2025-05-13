ce243a7d14a2d9d5bf38dc4713d5227886dc020d
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-05-14 00:31:53 +0200

    chore: restructure and change file names to be more chohesive

--- src/pages/Venues.tsx
+++ src/pages/Venues.tsx
@@ -1,16 +1,12 @@
-
 import DiplayVenues from "../components/VenueDetails";
-import SearchBar from "../components/search";
-
+import SearchBar from "../components/Search";
 
 function AllVenues() {
   console.log("All Venues");
   return (
     <>
       <section>
-
         <SearchBar />
-
       </section>
       <section>
         <DiplayVenues />
