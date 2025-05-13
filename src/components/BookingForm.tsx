dd8409bca571a9a335b8eab9dbef83a066353632
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-05-14 00:32:23 +0200

    feat: add booing logic

---
+++ src/components/BookingForm.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+
+interface BookingFormProps {
+  venueId: string;
+  maxGuests: number;
+  onBookingSuccess: () => void;
+}
+
+const BookingForm: React.FC<BookingFormProps> = ({
+  venueId,
+  maxGuests,
+  onBookingSuccess
+}) => {
+  const [dateFrom, setDateFrom] = useState("");
+  const [dateTo, setDateTo] = useState("");
+  const [guests, setGuests] = useState(1);
+  const apiKey = import.meta.env.VITE_X_NOROFF_API_KEY;
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const token = localStorage.getItem("bearerToken");
+
+    if (!token) {
+      alert("You need to be logged in to make a booking.");
+      return;
+    }
+
+    const dateFromISO = new Date(dateFrom).toISOString();
+    const dateToISO = new Date(dateTo).toISOString();
+
+    try {
+      const response = await fetch(
+        "https://v2.api.noroff.dev/holidaze/bookings",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+            "X-Noroff-API-Key": `${apiKey}`
+          },
+          body: JSON.stringify({
+            dateFrom: dateFromISO,
+            dateTo: dateToISO,
+            guests,
+            venueId
+          })
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(`Error: ${errorData.errors[0]?.message}`);
+        return;
+      }
+
+      const data = await response.json();
+      console.log("Booking confirmed:", data.data);
+      onBookingSuccess();
+    } catch (error) {
+      console.error("Booking-error:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="dateFrom">From:</label>
+        <input
+          type="date"
+          id="dateFrom"
+          name="dateFrom"
+          value={dateFrom}
+          onChange={(event) => {
+            setDateFrom(event.target.value);
+            console.log(dateFrom);
+          }}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="dateTo">To:</label>
+        <input
+          type="date"
+          id="dateTo"
+          name="dateTo"
+          value={dateTo}
+          onChange={(event) => {
+            setDateTo(event.target.value);
+            console.log(dateFrom);
+          }}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="guests">Guests:</label>
+        <select
+          id="guests"
+          name="guests"
+          value={guests}
+          onChange={(event) => setGuests(Number(event.target.value))}
+          required
+        >
+          {Array.from({ length: maxGuests }, (_, index) => (
+            <option key={index + 1} value={index + 1}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button type="submit">Book</button>
+    </form>
+  );
+};
+
+export default BookingForm;
--- src/pages/VenueDetail.tsx
+++ src/pages/VenueDetail.tsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Venue from "../types/Venue";
+import BookingForm from "../components/BookingForm";
+import { useNavigate } from "react-router-dom";
 
 function VenueDetail() {
   const { id } = useParams<{ id: string }>();
   const [venue, setVenue] = useState<Venue | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function getVenue(url: string) {
@@ -23,13 +26,19 @@ function VenueDetail() {
   if (!venue) {
     return <div>Loading venue details...</div>;
   }
+
+  const handleBookingSuccess = () => {
+    navigate("/dashboardUser");
+  };
+
   console.log("Venue in detail");
+
   return (
     <>
       <h1>{venue.name}</h1>
       <div>
         <p>{venue.location.city}</p>
-        <p>{venue.price}</p>
+        <p>{venue.price} kr pr night</p>
       </div>
       <div>
         <p>{venue.description}</p>
@@ -38,68 +47,42 @@ function VenueDetail() {
       <div>
         <h2>Amenities</h2>
         <div>
-          <img src="" alt="" />
+          <i className="fa-light fa-user-group-simple"></i>
           <p>Guests:</p>
           <p>{venue.maxGuests}</p>
         </div>
         {venue.meta.wifi && (
           <div>
-            <img src="" alt="" />
+            <i className="fa-light fa-wifi"></i>
             <p>Wifi</p>
           </div>
         )}
         {venue.meta.parking && (
           <div>
-            <img src="" alt="" />
+            <i className="fa-light fa-square-parking"></i>
             <p>Parking</p>
           </div>
         )}
         {venue.meta.breakfast && (
           <div>
-            <img src="" alt="" />
+            <i className="fa-light fa-bread-slice-butter"></i>
             <p>Breakfast</p>
           </div>
         )}
         {venue.meta.pets && (
           <div>
-            <img src="" alt="" />
+            <i className="fa-light fa-dog"></i>
             <p>Pets allowed</p>
           </div>
         )}
       </div>
       <div>
         <h2>Booking</h2>
-        <form action="">
-          <div>
-            <h3>Dates</h3>
-            <label htmlFor="">From</label>
-            <input type="date" id="dateFrom" name="dateFrom" />
-            <label htmlFor="">To</label>
-            <input type="date" id="dateTo" name="dateTo" />
-          </div>
-
-          <div>
-            <h3>Guests</h3>
-            <label htmlFor="guests">Guests</label>
-            <select name="guests" id="guests">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-          <div>
-            <h3>Price</h3>
-            <p>{venue.price}</p>
-          </div>
-          <button>Book venue</button>
-        </form>
+        <BookingForm
+          venueId={venue.id}
+          maxGuests={venue.maxGuests}
+          onBookingSuccess={handleBookingSuccess}
+        />
       </div>
     </>
   );
