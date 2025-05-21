38233d4b4da9601a2062c99043b90d5bd78e8b77
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-05-21 14:42:32 +0200

    add profile type and manage edit profile

---
+++ src/types/Profile.ts
@@ -0,0 +1,18 @@
+export type Profile = {
+  name: string;
+  email: string;
+  bio: string;
+  avatar: {
+    url: string;
+    alt: string;
+  };
+  banner: {
+    url: string;
+    alt: string;
+  };
+  venueManager: boolean;
+  _count: {
+    venues: number;
+    bookings: number;
+  };
+};
